d1b41aecc6704cddf9717365fc0e9d059d4bbed4
timdouglas <github@timdouglas.co.uk>
2013-08-16 16:58:48 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,59 @@
-grunt-livereload-snippet
-========================
+# grunt-livereload-snippet
 
-grunt task to add the livereload javascript snippet to an html page
+> Add livereload js snippet to html
+
+## Getting Started
+This plugin requires Grunt `~0.4.1`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-livereload-snippet --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-livereload-snippet');
+```
+
+## The "livereload_snippet" task
+
+### Overview
+In your project's Gruntfile, add a section named `livereload_snippet` to the data object passed into `grunt.initConfig()`.
+
+```js
+grunt.initConfig({
+  livereload_snippet: {
+    options: {
+      // Task-specific options go here.
+    },
+    your_target: {
+      // Target-specific file lists and/or options go here.
+    },
+  },
+})
+```
+
+### Options
+
+#### options.hostname
+Type: `String`
+Default value: `localhost`
+
+The hostname for your livereload server.
+
+#### options.port
+Type: `String`
+Default value: `35729`
+
+Port for livereload server.
+
+#### options.file
+Type: 'String'
+Default value: 'index.html'
+
+Relative path (from grunt) to the file that will get the snippet
+
+## Release History
+_(Nothing yet)_
---
+++ tasks/livereload_snippet.js
@@ -0,0 +1,64 @@
+/*
+ * grunt-livereload-snippet
+ * 
+ *
+ * Copyright (c) 2013 timdouglas
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('livereload_snippet', 'Add livereload js snippet to html', function() {
+    var fs = require('fs'),
+      done = this.async(),
+      options = this.options({
+        port: 35729,
+        hostname: 'localhost',
+        file: 'index.html'
+      }),
+      file = options.file,
+      contents,
+      snippet_regex = /<script src\=\"\/\/.*\:[0-9]+\/livereload\.js\?snipver\"\><\/script>/gi,
+      snippet = '<script src="//'+options.hostname+':'+options.port+'/livereload.js?snipver=1"></script>';
+
+    console.log("FILE IS ", this,file);
+
+    //check the file is there...
+    fs.exists(file, function(exists){
+      if(exists) {
+        // get file contents
+        contents = grunt.file.read(file);
+
+        if(!contents) {
+          grunt.warn("could not open file "+file);
+          done(false);
+        }
+
+        //remove existing and replace
+        contents = contents.replace(snippet_regex, '');
+        contents += snippet;
+
+        var res = grunt.file.write(file, contents);
+
+        if(res) {
+          grunt.log.writeln("Added livereload snippet to "+file);
+          done();
+        }
+        else {
+          grunt.warn("could not write to file "+file);
+          done(false);
+        }
+      }
+      else {
+        grunt.warn("file "+file+" does not exist");
+        done(false);
+      }
+    });    
+  });
+
+};
