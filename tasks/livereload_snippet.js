b8281f9f83f0d12eae620e2a26c06a51ed04718c
Tim Douglas <mail@timdouglas.co.uk>
2014-02-06 11:47:12 +0000

    allow multiple files to be passed to task
    uses standard grunt src: ['first/file/path.ext', 'second/file/path.ext'] syntax

--- tasks/livereload_snippet.js
+++ tasks/livereload_snippet.js
@@ -12,36 +12,37 @@ module.exports = function(grunt) {
   grunt.registerMultiTask('livereload_snippet', 'Add livereload js snippet to html', function() {
     var fs = require('fs'),
       path = require('path'),
-      done = this.async(),
       options = this.options({
         port: 35729,
         hostname: 'localhost',
-        file: 'index.html',
         add: true,
         after: undefined,
         before: undefined
       }),
-      file = path.resolve(options.file),
       contents,
       res,
-      snippet_regex = /<script src\=\"http\:\/\/.*\:[0-9]+\/livereload\.js\?snipver\=1\"\><\/script>/gi,
-      snippet = '<script src="http://'+options.hostname+':'+options.port+'/livereload.js?snipver=1"></script>';
+      err = false,
+      snippet_regex = /<script src\=\"\/\/.*\:[0-9]+\/livereload\.js\?snipver\=1\"\><\/script>/gi,
+      snippet = '<script src="//'+options.hostname+':'+options.port+'/livereload.js?snipver=1"></script>';
 
-    //check the file is there...
-    fs.exists(file, function(exists) {
 
-      if(exists === true) {
-        // get file contents
+    this.files.forEach(function(f) {
+      //filter out missing files
+      var src = f.src.filter(function(path) {
+        if (!grunt.file.exists(path)) {
+          grunt.log.warn('Source file "' + path + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(file) {
+        //get file contents
         contents = grunt.file.read(file);
 
-        if(contents === undefined) {
+        if(!contents) {
           grunt.warn("could not open file "+file);
-          done(false);
-        }
-        
-        if(contents.length === 0 && options.add == false) {
-          grunt.log.writeln(file+" is empty, done.");
-          done();
+          err = true;
+          return true;
         }
 
         //remove any existing snippets
@@ -50,7 +51,7 @@ module.exports = function(grunt) {
         //add snippet if allowed...
         if(options.add === true) {
           if(options.after !== undefined) {
-            contents = contents.replace(new RegExp(options.after, "g"), options.after + snippet);
+            contents = contents.relace(new RegExp(options.after, "g"), options.after + snippet);
           } else if(options.before !== undefined) {
             contents = contents.replace(new RegExp(options.before, "g"), snippet + options.before);
           } else {
@@ -66,17 +67,13 @@ module.exports = function(grunt) {
           } else {
             grunt.log.writeln("Remove livereload snippet from "+file);
           }
-
-          done();
         }
         else {
           grunt.warn("could not write to file "+file);
-          done(false);
+          err = true
+          return false;
         }
-      } else {
-        grunt.warn("file "+file+" does not exist");
-        done(false);
-      }
+      });
     });
   });
 };
